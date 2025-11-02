36be1a75dcc84ae30750d6bc61d07776eed85321
Martijn <144214561+MartijnVerhappen@users.noreply.github.com>
2025-11-02 15:57:58 +0100

    fixing MIME type error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/martijnverhappen-portfolio/',
   plugins: [react(), tailwindcss()],
+  base: "/MartijnVerhappen.github.io/"
 })
