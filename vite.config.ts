72cb9304b050cbc35e15e1e582f2947b363a443d
Martijn <144214561+MartijnVerhappen@users.noreply.github.com>
2025-11-02 16:15:32 +0100

    changing config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://martijnverhappen.github.io/"
+  base: "https://martijnverhappen.github.io/martijnverhappen-portfolio/"
 })
